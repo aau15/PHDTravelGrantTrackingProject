15a250e1c4d228232cf97012546ae47d0f344a2c
ShinningFinger <xian.zhu16@imperial.ac.uk>
2016-10-19 12:10:50 +0100

    Add files via upload
    Login and SignUp functions of the system. Created By Xavier. Due to the database connection problems, some test cases have not been tested. But I believe the main function works: ) (12:10 19/10/2016)

---
+++ SignIn.php
@@ -0,0 +1,58 @@
+
+<html>
+<body>
+<?php
+$servername = "mysql5006.smarterasp.net";
+$username = "a117c6_phd";
+$password = "phd12345";
+
+
+@session_start();
+$security_token = $_SESSION['security_token'] = uniqid(rand());
+
+
+
+if(! isset($_POST["Submit"]) ){
+$Id = $_POST["Id"];
+$passWord = $_POST["passWord"];
+    if($Id == "" || $passWord == "") {
+        echo "<script>alert('Please enter ID or password'); history.go(-1);</script>";
+    }
+    else{
+        $conn = new mysqli($servername, $username, $password);
+        if ($conn->connect_error) {
+            die("Failed " . $conn->connect_error);
+        }
+        $sql = "SELECT StudentID FROM db_a117c6_phd.student WHERE StudentID = '$_POST[Id]' and PassWord = '$_POST[passWord]' ";
+        $result = $conn->query($sql);
+        $num = mysqli_num_rows($result);
+
+
+        if ($num ) {
+//            $_SESSION["StudentID"]=$_POST["Id"];
+//            $sql2 = "SELECT StudentID  FROM db_a117c6_phd.student WHERE StudentID = '$_SESSION[StudentID]'";
+//            $result = $conn->query($sql2);
+//            $t_result = mysqli_fetch_array($result);
+
+//            if($result==0 || $t_result==0){
+//                echo "<script>alert('Incorrect email or password!');history.go(-1);</script>";
+//            }
+//            else{
+//                $_SESSION["StudentID"]=$t_result[0];
+//                $_SESSION["PassWord"]=$t_result[1];
+                echo "<script>window.location='index.html';</script>";
+//            }
+        }
+        else {
+            echo "<script>alert('Incorrect ID or password!');history.go(-1);</script>";
+        }
+
+        $conn->close();
+    }}
+
+?>
+
+
+</body>
+</html>
+
